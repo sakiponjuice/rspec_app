591d6c5e57b5a51cd047ccff5655380747bd7df9
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-11-30 07:31:56 +0000

    [Add] rubocop

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,20 +8,22 @@ RSpec.describe Post, type: :model do
       @post.content = "今日も晴れです。"
       @post.save
     end
+
     it "全て入力してあるので保存される" do
       expect(@post).to be_valid
     end
     context "データが正しく保存されない" do
-    before do
-      @post = Post.new
-      @post.title = ""
-      @post.content = "今日も晴れです。"
-      @post.save
+      before do
+        @post = Post.new
+        @post.title = ""
+        @post.content = "今日も晴れです。"
+        @post.save
+      end
+
+      it "titleが入力されていないので保存されない" do
+        expect(@post).to be_invalid
+        expect(@post.errors[:title]).to include("can't be blank")
+      end
     end
-    it "titleが入力されていないので保存されない" do
-      expect(@post).to be_invalid
-      expect(@post.errors[:title]).to include("can't be blank")
-    end
-   end
   end
 end
--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Posts", type: :request do
       before do
         get posts_new_path
       end
+
       it 'リクエストは200 OKとなること' do
         expect(response.status).to eq 200
       end
